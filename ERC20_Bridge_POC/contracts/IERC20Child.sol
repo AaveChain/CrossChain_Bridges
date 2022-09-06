4d29b9511b71931241b9089a44456f97ba36350c
AaveChaine <akchain22@gmail.com>
2022-09-06 20:39:39 +0530

    updated

--- ERC20_Bridge_POC/contracts/IERC20Child.sol
+++ ERC20_Bridge_POC/contracts/IERC20Child.sol
@@ -1,24 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//Remix style import
+// Remix style import
 import { IERC20 } from "@openzeppelin/contracts@4.0.0/token/ERC20/IERC20.sol";
 
 
-// Interface of the child ERC20 token, for use on sidechains and L2 networks.
+/// Interface of the child ERC20 token, for use on sidechains and L2 networks.
 interface IERC20Child is IERC20 {
   /**
-   * called by bridge gateway when tokens are deposited on root chain
+   *  called by bridge gateway when tokens are deposited on root chain
    * Should handle deposits by minting the required amount for the recipient
    *
    * recipient an address for whom minting is being done
    * amount total amount to mint
    */
-  function mint(
-    address recipient,
-    uint256 amount
-  )
-    external;
+  function mint(address recipient, uint256 amount) external;
 
   /**
    * called by bridge gateway when tokens are withdrawn back to root chain
@@ -26,16 +22,11 @@ interface IERC20Child is IERC20 {
    *
    * amount total amount to burn
    */
-  function burn(
-    uint256 amount
-  )
-    external;
+  function burn(uint256 amount) external;
 
   /**
-   *
    * account an address for whom burning is being done
    * amount total amount to burn
    */
   function burnFrom(address account, uint256 amount) external;
 }
-
